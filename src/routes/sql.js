b7ac6be3b840f589f759da66821206533313bc5e
Herbert Tung <herberttung@hotmail.com>
2020-05-06 02:36:52 +0000

    init

--- src/routes/sql.js
+++ src/routes/sql.js
@@ -1,18 +1,20 @@
-import express  from 'express'
-import mysql  from 'mysql'
+import express from 'express'
+import mysql from 'mysql'
+import oracledb from 'oracledb'
 
 const router = express.Router();
 
-router.get('/getSQL', function (req, res, next) {
+router.get('/getSQL', async (req, res, next) => {
+  /*
   const query = `SELECT * from Service
 `
 
   const con = mysql.createConnection({
-    host: '172.25.13.74',
-    port: 3306,
-    user: 'nttstest',
-    password: '777ntts',
-    database: 'CircuitDBLive',
+    host: '172.25.15.138',
+    port: 1521,
+    user: 'fbi',
+    password: 'abc123',
+    database: 'fbiu',
     insecureAuth: true
   })
 
@@ -24,6 +26,53 @@ router.get('/getSQL', function (req, res, next) {
       console.log(result)
     });
   });
+  const config = {
+    user: 'fbi',
+    password: 'abc123',
+    host: '172.25.15.138:1521',
+    database: 'fbiu'
+  }
+
+  client
+    .connect(config, () => {      
+      console.log('Connected!')    
+    })
+    /*.then((connection => {
+      console.log('Connected!')
+      return connection
+        .execute("select * from all_objects where object_name = 'JIRA_CPE_TYPE'")
+        .then(result => {
+          console.log('Queried!')
+          return connection
+            .close()
+            .then(() => result);
+        })
+        .catch(err => {
+          console.log(err)
+          return connection
+            .close()
+            .then(() => Promise.reject(err));
+        });
+    }))*/
+
+    oracledb.getConnection(
+      {
+        user          : "fbi",
+        password      : "abc123",
+        connectString : "172.25.15.138:1521/fbiu"
+      },
+      function(err, connection)
+      {
+        console.error('connected!')
+        if (err) { console.error(err); return; }
+        connection.execute(
+          "select * from AA",
+          function(err, result)
+          {
+            if (err) { console.error(err); return; }
+            console.log(result.rows);
+          });
+      });
 })
 
 module.exports = router;
